cd4d613155f70882ef73a0d74be795fa51912bf4
baldomarco <92460568+baldomarco@users.noreply.github.com>
2023-11-22 16:20:15 +0100

    Add files via upload

---
+++ SummerSchool/Interdisciplinary summer school.R
@@ -0,0 +1,72 @@
+
+install.packages("rtools")
+install.packages("tidyverse")   # Proclias # Bottoms up
+install.packages("purr")        # Bottoms up
+install.packages("adiv")        # Proclias # Bottoms up
+install.packages("RSQLite")     # Proclias
+install.packages("mice")        # Bottoms up
+install.packages("taxize")      # Bottoms up
+devtools::install_github("lmterryn/ITSMe", build_vignettes = TRUE)
+install.packages("ITSME")       # 3DForEcoTech
+install.packages("devtools")    # Proclias and # Bottoms up
+install.packages("usethis")     # Bottoms up # Bottoms up
+install.packages("truncnorm")   # Proclias
+install.packages("openxlsx")    # Proclias
+install.packages("cowplot")     # Proclias
+install.packages("treespat")    # Proclias install through devtools
+devtools::install_gitlab('fchianucci/treespat')
+install.packages("here")        # Proclias
+install.packages("ggplot2")     # Proclias
+install.packages("DHARMa")      # Proclias
+install.packages("dismo")       # Proclias
+install.packages("gridExtra")   # Proclias
+install.packages("vegan")       # Bottoms up
+install.packages("ape")         # Bottoms up
+install.packages("betapart")    # Bottoms up
+install.packages("BAT")         # Bottoms up  - Caio Graco-Roza
+install.packages("gdm")         # Bottoms up
+install.packages("tidymodels")
+
+# install.packages("devtools")
+devtools::install_github("lmterryn/ITSMe", build_vignettes = TRUE)
+
+
+library(tidyverse)
+library(purrr)
+library(adiv)
+library(RSQLite)
+library(mice)
+library(taxize)
+library(ITSMe)
+# library(devtools)
+library(truncnorm)
+library(openxlsx)
+library(cowplot)
+library(treespat)
+library(here)
+library(ggplot2)
+library(DHARMa)
+library(dismo)
+library(gridExtra)
+library(vegan)
+library(ape)
+library(betapart)
+library(BAT)
+library(gdm)
+# library(tidymodels)
+
+
+# bray curtis on precence absence is a sorrensen index
+
+# we will use "gower" instead then "euclidian" distance among sites
+
+# You can 
+
+
+
+
+
+
+
+
+
