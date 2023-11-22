e18afe025a742af30843692ef01899bd604d020b
baldomarco <92460568+baldomarco@users.noreply.github.com>
2023-11-22 18:22:36 +0100

    Update Biod_pred_table.r

--- STSM/BDV_tables/Biod_pred_table.r
+++ STSM/BDV_tables/Biod_pred_table.r
@@ -4,12 +4,19 @@
 
 # Jenik grid 
 
+# Marco Baldo 25-07-2023
+# Create a table of predictors of multi-taxa biodiversity indicators in forest ecosystem of Czechia
+
+
+# Jenik grid 
+
 library(readxl)
 
 
 # source data table:
 t1<-read_excel("C:/iLand/2023/20230901_Bottoms_Up/plot_init/Jenik/final_table_imp/Bdv_predictors_v2.xlsx")
 t2<-read_excel("C:/iLand/2023/20230901_Bottoms_Up/plot_init/Jenik/final_table_imp/Jenyk_BDV_predictors_ENG.xlsx")
+
 
 excel_sheets(t1) # how many sheets are there and their name in the excel file f.
 excel_sheets(t2)
@@ -23,16 +30,16 @@ library(tidyr)
 library(dplyr)
 library(RSQLite)
 
-setwd("C:/iLand/2023/20230901_Bottoms_Up/20230914_plot_experiment/_project/output/")
+setwd("I:/iLand/2023/20230901_Bottoms_Up/outputs/20231002/output/")
 
 # Path to the directory containing your SQLite databases
-dataroot <- "C:/iLand/2023/20230901_Bottoms_Up/20230914_plot_experiment/_project/output/"
+dataroot <- "I:/iLand/2023/20230901_Bottoms_Up/outputs/20231002/output/"
 
 # Get a list of all SQLite databases in the directory
 # database_files <- list.files(path = dataroot, pattern = ".sqlite", full.names = TRUE)
 
 {# Create an empty list to store data frames
-dfs <- list() # not working for several subset, only one
+  dfs <- list() # not working for several subset, only one
 }
 
 
@@ -71,7 +78,7 @@ for (i in (1:length(database_files)))  {    # We read in the files in the loop. 
   
   # i<-1 # to test but remember to don't run also the }
   
-
+  
   # Name of the database
   file <-paste0(database_files[i])                                    # File to read here the case is always the actual case in the loop
   # "file"= name of the object, "paste0"+ function to create a NAME for a computer path of selection of data/objects
@@ -79,7 +86,7 @@ for (i in (1:length(database_files)))  {    # We read in the files in the loop. 
   # case<- strsplit(all_v[i],".s")[[1]][1]      # Why we used it in the paper analysis? # explanation of this function is needed
   case<-database_files[i] 
   
-
+  
   # Control
   print(file)
   
@@ -128,7 +135,7 @@ for (i in (1:length(database_files)))  {    # We read in the files in the loop. 
   
   
 }
-  
+
 # Crete the LAI vector
 LAI <- data.frame (lnd %>%
                      group_by(run) %>%
@@ -136,7 +143,7 @@ LAI <- data.frame (lnd %>%
                      summarise(tot_volume=sum(volume_m3),living_c=sum(total_carbon_kg), count_ha=sum(count_ha), tot_ba=sum(basal_area_m2),npp=sum(NPP_kg), LAI=sum(LAI), sapling=sum(cohort_count_ha),growth_m3=sum(gwl_m3)))
 
 library(writexl)
-write_xlsx (LAI, paste0(dataroot,"CZ_plot_lnd.xlsx"))
+write_xlsx (LAI, paste0(dataroot,"CZ_plot_lnd_replicated_20231122.xlsx"))
 
 
 
@@ -146,7 +153,7 @@ library(GGally)
 library(corrplot)
 
 
-tab2 <- read_xlsx("C:/iLand/2023/20230901_Bottoms_Up/plot_init/Jenik/final_table_imp/Bdv_predictors_table_final_20231002.xlsx")
+tab2 <- read_xlsx("I:/iLand/2023/20230901_Bottoms_Up/plot_init/Jenik/final_table_imp/Bdv_predictors_table_final_20231002.xlsx")
 
 # head(tab1)
 head(tab2)
@@ -167,4 +174,5 @@ corrplot.mixed(cor(a.num),upper.col = col4(10),lower.col = "black", mar=c(0,0,0,
 #---------------------------------- just do the correlation plot with the selected variables
 
 ggpairs(a.num)
+
 
