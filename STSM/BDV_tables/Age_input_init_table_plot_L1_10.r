a6d78bfc6ae6b917b42190492f8565ee504e727b
baldomarco <92460568+baldomarco@users.noreply.github.com>
2023-11-23 18:32:28 +0100

    Update Age_input_init_table_plot_L1_10.r

--- STSM/BDV_tables/Age_input_init_table_plot_L1_10.r
+++ STSM/BDV_tables/Age_input_init_table_plot_L1_10.r
@@ -3,3 +3,40 @@ library(readxl)
 library(writexl)
 
 
+# Import the required tables
+age_L1_10 <- read_excel("C:/iLand/2023/20230901_Bottoms_Up/plot_init/Jenik/final_table_imp/Plot L1_10 TreeAgesDerivedfromYieldTables.xlsx")
+init_L1_10 <- read.table("C:/iLand/2023/20230901_Bottoms_Up/20230914_plot_experiment/_project/init/L1_10_init.txt", header = TRUE, sep = ";")
+
+# Look at them
+str(age_L1_10)
+head(age_L1_10)
+
+str(init_L1_10)
+head(init_L1_10)
+
+
+#-------------------------------------------------------------------------------
+# Manipulate the age dataframe to have only what we are interest to know = age and multiply by 4 to have the same number of lines that we have in init (remind we are synthesise information from forest field samples at 50mx50m are in iLand model at 100mx100m area, that's why we repeat the trees x4)
+
+filtered_age_L1_10 <- age_L1_10[, 12]
+
+# Replicate each value four times
+rep_age <- rep(filtered_age_L1_10$`mean age`, each = 4)
+
+# Round just for the integers
+age <- as.integer(round(as.numeric(rep_age)))
+
+# Create a new dataframe with the replicated values in a single column
+new_age_L1_10 <- data.frame(age)
+
+# Print the new dataframe
+print(new_age_L1_10)
+
+mean(new_age_L1_10$age)
+#-------------------------------------------------------------------------------
+# Combind columns between the init_L1_10 dataframe and age
+init_L1_10 <- bind_cols(init_L1_10, new_column = new_age_L1_10$age)
+
+# Write the updated data to the same path
+write.table(init_L1_10, file = "C:/iLand/2023/20230901_Bottoms_Up/20230914_plot_experiment/_project/init/L1_10_init_age.txt", sep = ";", row.names = FALSE)
+
